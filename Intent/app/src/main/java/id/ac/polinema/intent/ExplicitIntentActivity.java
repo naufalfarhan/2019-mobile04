3495abc599d7a25a0c38b4dc8f9146554586a682
naufalfarhan <naufalfarhannn@gmail.com>
2019-11-04 09:30:51 +0700

    intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,20 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText teksInput;
+    private TextView teksOutput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        teksInput = findViewById(R.id.input_name);
+        teksOutput = findViewById(R.id.text_output);
     }
 
+
+    public void handleSubmit(View view) {
+        String teks = teksInput.getText().toString();
+        teksOutput.setText("Hallo" + teks);
+    }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -2,21 +2,33 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import model.User;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_parcelable);
 
-        // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
+        User user = getIntent().getParcelableExtra("USER");
 
-            // TODO: display value here
+        if(user != null) {
+            usernameText.setText(user.getUsername());
+            nameText.setText(user.getName());
+            ageText.setText(String.valueOf(user.getAge()));
         }
     }
 }
